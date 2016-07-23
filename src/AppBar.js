7ecd11df2660fb4491a0202f123d685490d2c0d9
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-07-23 16:32:50 -0400

    make the title green

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
+import {white} from 'material-ui/styles/colors';
+
+import IconButton from 'material-ui/IconButton';
+import PrintIcon from 'material-ui/svg-icons/action/print'
+
+
 class AppBarExample extends React.Component {
 	render() {
 		return (
-			<AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more"></AppBar>
+			<AppBar title="NearPrinter"
+					iconElementLeft={<IconButton><PrintIcon /></IconButton>}
+					iconClassNameRight="muidocs-icon-navigation-expand-more"></AppBar>
 	    )
 	}
 }
--- src/app.js
+++ src/app.js
@@ -6,14 +6,22 @@ import MapContainer from './MapContainer'
 import AppBar from './AppBar'
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {green500} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const muiTheme = getMuiTheme({
+  appBar: {
+    height: 50,
+    color: green500
+  }
+});
 
 // Required for material-ui
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
 ReactDOM.render(
-	<MuiThemeProvider muiTheme={getMuiTheme()}>
+	<MuiThemeProvider muiTheme={muiTheme}>
 		<div>
 			<AppBar />
 			<MapContainer />
