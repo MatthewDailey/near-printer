10abe0d92ed605bb501bf1a27e3c3eef266311a7
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-07-24 23:48:28 -0400

    add easy print button

--- src/app.js
+++ src/app.js
@@ -7,6 +7,66 @@ import MapContainer from './MapContainer'
 import RaisedButton from 'material-ui/RaisedButton';
 
 
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
+const customContentStyle = {
+  width: '100%',
+  maxWidth: 'none',
+};
+
+/**
+ * The dialog width has been set to occupy the full width of browser through the `contentStyle` property.
+ */
+class EasyPrintButton extends React.Component {
+	constructor(props) {
+    	super(props);
+    	this.state = {isEasyPrintDialogOpen: false};
+    	this.handleEasyButtonClicked = this.handleEasyButtonClicked.bind(this);
+    	this.handleClose = this.handleClose.bind(this);
+  	}
+
+  	handleEasyButtonClicked() {
+    	this.setState({isEasyPrintDialogOpen: true});
+  	};
+
+  	handleClose() {
+
+    	this.setState({isEasyPrintDialogOpen: false});
+  	};
+
+  	render() {
+	    const actions = [
+	      <FlatButton
+	        label="Cancel"
+	        primary={true}
+	        onTouchTap={this.handleClose}
+	      />,
+	      <FlatButton
+	        label="Submit"
+	        primary={true}
+	        onTouchTap={this.handleClose}
+	      />,
+	    ];
+
+	    return (
+	      <div>
+	        <RaisedButton label="The Easy Print Button" onTouchTap={this.handleEasyButtonClicked} style={fixedElementsStyle} />
+	        <Dialog
+	          title="Dialog With Custom Width"
+	          actions={actions}
+	          modal={true}
+	          contentStyle={customContentStyle}
+	          open={this.state.isEasyPrintDialogOpen}
+	        >
+	          This dialog spans the entire width of the screen.
+	        </Dialog>
+	      </div>
+	    );
+  	}
+}
+
+
 const containerStyle = {
 	position: "relative",
 	width: '100vw',
@@ -28,5 +88,6 @@ const handleClickedEasyButton = () => {
 RenderReactApp(
 	<div style={containerStyle}>
 		<MapContainer />
-		<RaisedButton label="The Easy Print Button" onClick={handleClickedEasyButton} style={fixedElementsStyle} />
+
+		<EasyPrintButton />
 	</div>)
