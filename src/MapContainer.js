aed70f3d3c61d003f6eb25ed28c36f20113a58d9
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-07-25 01:43:28 -0400

    show markers for printers

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -7,7 +7,7 @@ class MapContainer extends React.Component {
 	render() {
 		const style = {
 			width: '100vw',
-			height: '100vh',
+			height: '100%',
 			position: 'absolute'
 		}
 		return (
@@ -39,18 +39,59 @@ class MapContainer extends React.Component {
 			});
 
 
-			this.map = new maps.Map(node, mapConfig);
+			const map = new maps.Map(node, mapConfig);
 
-			const easyButtonMarker = new google.maps.Marker({
-			    position: {lat, lng},
-			    map: this.map,
-			    title: 'Hello World!',
-			    animation: google.maps.Animation.DROP
+	        if (window.Promise) {
+			  console.log('Promise found');
+
+			  var promise = new Promise(function(resolve, reject) {
+			    var request = new XMLHttpRequest();
+
+			    request.open('GET', '/getPrinters');
+			    request.onload = function() {
+			      if (request.status == 200) {
+			        resolve(request.response); // we got data here, so resolve the Promise
+			      } else {
+			        reject(Error(request.statusText)); // status is not 200 OK, so reject
+			      }
+			    };
+
+			    request.onerror = function() {
+			      reject(Error('Error fetching data.')); // error occurred, reject the  Promise
+			    };
+
+			    request.send(); //send the request
 			  });
 
-			easyButtonMarker.addListener('click', function() {
-		        alert("clicked easyButton")
-	        });
+			  console.log('Asynchronous request made.');
+
+			  promise.then(function(data) {
+			    console.log('Got data! Promise fulfilled.');
+			    const printers = JSON.parse(data);
+
+			    for (let printerKey in printers) {
+			    	var printer = printers[printerKey];
+			    	console.log(printer)
+			    	lat = printer.lat;
+			    	lng = printer.lon;
+			    	var printerPosition = {lat, lng}
+
+					const easyButtonMarker = new google.maps.Marker({
+					    position: printerPosition,
+					    map: map,
+					    title: 'Hello World!',
+					    animation: google.maps.Animation.DROP
+					});
+			    }
+
+			  }, function(error) {
+			    console.log('Promise rejected.');
+			    console.log(error.message);
+			  });
+			} else {
+			  console.log('Promise not available');
+			}
+
 
 		}
 	}
